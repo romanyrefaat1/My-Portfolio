5ae967228fabf265279e3e87fda04a0ac968b821
Romany Refaat <romanyrefaat641l@gmail.com>
2026-09-11 20:37:57 +0300

    Added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -47,6 +48,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
       <body className="font-sans antialiased">
+        <Analytics />
         <ThemeProvider themes={["dark"]}>
         {children}
         </ThemeProvider>
--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
 
 export function SiteFooter() {
-  return <footer><div className="container footer-inner"><span>Romani — © 2026</span><div className="footer-links"><Link href="https://github.com/Romanirefaat1" target="_blank">GitHub</Link><Link href="https://x.com/Romanirefaat_" target="_blank">X</Link><Link href="https://www.linkedin.com/in/Romani-refaat-5a3ab0357/" target="_blank">LinkedIn</Link></div></div></footer>;
+  return <footer><div className="container footer-inner"><span>Romani — © 2026</span><div className="footer-links"><Link href="https://github.com/romanyrefaat1" target="_blank">GitHub</Link><Link href="https://x.com/romanyrefaat_" target="_blank">X</Link><Link href="https://www.linkedin.com/in/romany-refaat-5a3ab0357/" target="_blank">LinkedIn</Link></div></div></footer>;
 }
